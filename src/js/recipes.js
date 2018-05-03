2fa83fb04584a833e4f4446d17f6a5bea5869708
Grayson Erhard <grayson@graphiclux.com>
2018-05-03 14:22:34 -0600

    no idea

--- src/js/recipes.js
+++ src/js/recipes.js
@@ -29,14 +29,13 @@ $( document ).ready(function() {
   /******** Product Slider Recipe ******/
 
   // fadeIn
-  $('.home hr').addClass('wp-animation').addClass('wp-fadeIn').addClass('wp-delay-1');
-  $('.home .ge-slider-product-title').addClass('wp-animation').addClass('wp-fadeIn').addClass('wp-delay-1');
-  $('.home .ge-slider-product-price').addClass('wp-animation').addClass('wp-fadeIn').addClass('wp-delay-2');
+  $('.home .ge-slider-product-title').addClass('wp-animation').addClass('wp-fadeIn');
+  $('.home .ge-slider-product-price').addClass('wp-animation').addClass('wp-fadeIn');
   $('.home .ge-slider-product-headline.desktop').addClass('wp-animation').addClass('wp-fadeIn');
   // fadeInUp
   $('.home .ge-slider-product').addClass('wp-animation').addClass('wp-fadeInUp');
   $('.home .ge-slider-product-headline.mobile').addClass('wp-animation').addClass('wp-fadeInUp');
-  $('.home .ge-slider-add-to-cart').addClass('wp-animation').addClass('wp-fadeInUp').addClass('wp-delay-2');
+  $('.home .ge-slider-add-to-cart').addClass('wp-animation').addClass('wp-fadeInUp');
 
 
   $('.product-background.record').addClass('wp-animation').addClass('wp-rotateIn').addClass('wp-delay-1');
